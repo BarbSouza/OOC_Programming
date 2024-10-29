e0569c5191a501d53ac637b458a649a97bf47f0e
Barbs28 <2023059@student.cct.ie>
2024-10-29 09:47:06 +0000

    Create InvalidLengthException class

---
+++ scr/projects/Exceptions/Exceptionals/src/exceptionals/InvalidLengthException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exceptionals;
+
+/**
+ *
+ * @author Barbara
+ */
+public class InvalidLengthException extends Exception{
+    
+    public InvalidLengthException(){}
+    
+    public InvalidLengthException(String message){
+        super(message);
+    }
+            
+}
