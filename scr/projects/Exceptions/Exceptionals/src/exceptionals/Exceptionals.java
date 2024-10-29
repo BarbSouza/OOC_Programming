79644fc2b8aaa5cc789bc47d31696658499f4b42
Bárbara Candido de Souza <barbara.candido.souza@gmail.com>
2024-10-29 10:36:22 +0000

    Update Exceptionals.java The first exception block for circle

--- scr/projects/Exceptions/Exceptionals/src/exceptionals/Exceptionals.java
+++ scr/projects/Exceptions/Exceptionals/src/exceptionals/Exceptionals.java
@@ -14,7 +14,35 @@ public class Exceptionals {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Taking in the user input
+        //
+        
+        Scanner sc = new Scanner(System.in);
+        
+        boolean validInput = false;
+        Circle circle1;
+        //We need to take the input for the circle
+        
+        int input = 0;
+
+        //the first exception block 
+        while(!validInput){ //check the user input and prompt over and over 
+            try{
+            //Ask the user what you want to store
+            System.out.println("What is the radius of the cricle?");
+            input = sc.nextInt();
+            circle1 = new Circle(input);
+            validInput = true;
+            }catch(InvalidLengthException e){
+                System.out.println(e);
+            }catch(NegativeNumberException e){
+                System.out.println(e);
+            }
+                
+        }
+
     }
+    
+}
     
 }
