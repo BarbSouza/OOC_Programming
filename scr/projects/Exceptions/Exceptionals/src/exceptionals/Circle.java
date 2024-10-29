d813c47f381bc08812a343fbe3b5d5b9f3c282cc
Bárbara Candido de Souza <barbara.candido.souza@gmail.com>
2024-10-29 09:58:13 +0000

    Update Circle.java
    Adding the exceptions to Circle class

--- scr/projects/Exceptions/Exceptionals/src/exceptionals/Circle.java
+++ scr/projects/Exceptions/Exceptionals/src/exceptionals/Circle.java
@@ -9,5 +9,26 @@ package exceptionals;
  * @author Barbara
  */
 public class Circle {
+        //The Circle must have a radius
+        //We need to validate the inputs
+        //
+    
+    //Construct the attributes
+    int radius; //attribute
+    
+    //constructor to assign or retrieve the value
+    
+    public Circle(int radius) throws ZeroLengthException{ // //return type
+        //The condition to text for our exception
+        if(radius == 0){
+            //if 0 throw an exception message
+            throw new ZeroLengthException();
+            
+        }else if(radius < 0){
+            throw new NegativeNumberException("Radius cannot be a negative number");
+        }
+        //otherwise assign the radius value to the attribute
+        this.radius = radius;
+    }
     
 }
