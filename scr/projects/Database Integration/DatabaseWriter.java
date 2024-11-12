e135c1cdefe98358ec3eb39ea3510120567f7187
Bárbara Candido de Souza <barbara.candido.souza@gmail.com>
2024-11-12 11:02:18 +0000

    Update DatabaseWriter.java
    Instert data into table

--- scr/projects/Database Integration/DatabaseWriter.java
+++ scr/projects/Database Integration/DatabaseWriter.java
@@ -9,5 +9,32 @@ package db_connection;
  * @author Barbara
  */
 public class DatabaseWriter {
+    //This method will write information to the database
+    
+    public boolean addPatient(Patient patient) throws SQLException {
+        //The patient instance == 
+        try(
+                 Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+                 Statement stmt = conn.createStatement();
+                 ){
+            
+            //Instert data into table INSERT INTO TABLE cols VALUES
+            String sql = String.format("INSERT INTO " + TABLE + "VALUES {"
+            + "'%s', '%s', '%s', %d);",
+                    patient.getName(),
+                    patient.getBirthDate(),
+                    patient.getBloodType(),
+                    patient.getPatientID()
+                    
+            );
+            stmt.execute(sql);
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+    }
+    }
+    
+    
     
 }
