ef3e0324ed6a563724ee1a6822be98cba1afa0f1
Bárbara Candido de Souza <barbara.candido.souza@gmail.com>
2024-11-12 11:40:59 +0000

    Update HospitalAdministration.java

--- scr/projects/Database Integration/HospitalAdministration.java
+++ scr/projects/Database Integration/HospitalAdministration.java
@@ -10,4 +10,23 @@ package db_connection;
  */
 public class HospitalAdministration {
     
+    //The main method will try to insert data into db
+    //check connection status
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{
+        
+        //Using the db set up we can check if the operation went ok
+        
+        if(DatabaseSetup.setupDB()){
+        //If after we run the setupDB method all is ok
+        //we have now created a db schema and create a table
+        
+            System.out.println("Database has been successfully or already exists.");
+        }else {
+            //This means there is an issue either connecting to the db or creating it
+            System.out.println("There was a problem creating or connecting to the databases. \nPlease check the db credentials");
+            return;
+        }
+
+    }
+    
 }
